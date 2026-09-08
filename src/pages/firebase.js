8404ae805a0e7955b719c5f8fb96ac9d0dcfd12b
Bhumika Nathawat <bhumikanathawat0101@gmail.com>
2026-09-08 11:16:45 +0530

    Refactor comments in firebase.js for clarity
    Updated comments for clarity in Firebase configuration.

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -4,15 +4,15 @@ import { getAuth } from "firebase/auth";
 
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// You can find this in your Firebase Console: Project settings -> General -> Your apps -> select your web app -> "Config" button
+// web app's Firebase configuration
+//Firebase Console: Project settings -> General -> Your apps -> select your web app -> "Config" button
 const firebaseConfig = {
-  apiKey: "AIzaSyDnJEKsyEvr57O8e2m7otQWf9xwWzvI-wY", // Replace with your actual API Key
-  authDomain: "conecta-11ed1.firebaseapp.com", // This should be your auth domain
-  projectId: "conecta-11ed1", // Your project ID
-  storageBucket: "conecta-11ed1.appspot.com", // Your storage bucket
-  messagingSenderId: "182515030623", // Replace with your sender ID
-  appId: "1:182515030623:web:4396df22caaffa634ed9ea" // Replace with your App ID
+  apiKey: "AIzaSyDnJEKsyEvr57O8e2m7otQWf9xwWzvI-wY", //  actual API Key
+  authDomain: "conecta-11ed1.firebaseapp.com", //  auth domain
+  projectId: "conecta-11ed1", // project ID
+  storageBucket: "conecta-11ed1.appspot.com", // storage bucket
+  messagingSenderId: "182515030623", //  sender ID
+  appId: "1:182515030623:web:4396df22caaffa634ed9ea" //  App ID
 };
 
 // Initialize Firebase
